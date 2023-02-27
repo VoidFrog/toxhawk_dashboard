dc2ca3dd2495af62ff86b326ed250811e5d64da4
ShadowN0 <ShadowNN0000@gmail.com>
2023-02-27 21:29:44 +0100

    more styling, changed display for dataList to grid

--- src/components/DataDisplay/DataList/DataList.tsx
+++ src/components/DataDisplay/DataList/DataList.tsx
@@ -6,12 +6,12 @@ export default function DataList() {
   return (
     <div className='data-list-container unselectable'>
       <div className='data-list-titles'>
-        <div style={{flex:0}}><p className='fw700 size16'>Select</p></div>
-        <div style={{flex:2}}><p className='fw700 size16'>Sample&nbsp;name</p></div>
-        <div style={{flex:4}}><p className='fw700 size16'>Location</p></div>
-        <div style={{flex:2}}><p className='fw700 size16'>Rating</p></div>
-        <div style={{flex:2}}><p className='fw700 size16'>Pollutants</p></div>
-        <div style={{flex:4}}><p className='fw700 size16'>Time</p></div>
+        <div className='data-list-grid-a'><p className='fw700 size16'>Select</p></div>
+        <div className='data-list-grid-b'><p className='fw700 size16'>Sample&nbsp;name</p></div>
+        <div className='data-list-grid-c'><p className='fw700 size16'>Location</p></div>
+        <div className='data-list-grid-d'><p className='fw700 size16'>Rating</p></div>
+        <div className='data-list-grid-e'><p className='fw700 size16'>Pollutants</p></div>
+        <div className='data-list-grid-f'><p className='fw700 size16'>Time</p></div>
       </div>
       <DataListItem />
     </div>
--- src/components/DataDisplay/DataListItem/DataListItem.tsx
+++ src/components/DataDisplay/DataListItem/DataListItem.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
 import './DataListItem.css'
+import mapIcon from '../../../assets/map-icon.svg' 
+
+import Colors from '../../UI/Colors/Colors'
+import Button from '../../UI/Buttons/Button'
+import ButtonData from '../../../dataModels/ButtonData/ButtonData'
 
 export default function DataListItem() {
+  let [isWideEnough, changeSize] = React.useState(window.innerWidth>1700 ? true : false)
+
+  window.addEventListener('resize', ()=> {
+    changeSize(window.innerWidth>1700 ? true : false)
+  })
+
+  let btnData:ButtonData = {
+    title: 'Details',
+    size: isWideEnough
+      ? 'large'
+      : 'medium',
+    color: Colors.blue,
+    function: () => console.log('details work')
+  }
+
   return (
-    <div>DataListItem</div>
+    <div className='data-list-item-container'>
+      <div className='fw700 size16 data-list-item-grid-a'><input type="checkbox" name="" id=""/></div>
+      <div className='fw700 size16 data-list-item-grid-b'><p>nazwa miejsca</p></div>
+      <div className='data-list-item-grid-c'><img src={mapIcon} alt=""/></div>
+      <div className='fw700 size16 data-list-item-grid-d'><p>rating</p></div>
+      <div className='fw700 size16 data-list-item-grid-e'><p>pollutants</p></div>
+      <div className='fw700 size16 data-list-item-grid-f'><p>timestamp</p></div>
+      <div className='data-list-item-grid-g'><Button {...btnData}/></div>
+    </div>
   )
 }
